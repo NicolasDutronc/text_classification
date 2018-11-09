6b5c16e3068cb8d41c38a1419dfa3f63ede0cca7
NicolasDutronc <nicolas.dutronc@orange.fr>
2018-11-09 03:32:47 +0100

    feat(CNN): ajout du modèle basé sur un CNN

--- models.py
+++ models.py
@@ -24,3 +24,28 @@ class SimpleModel(nn.Module):
         # print('pred shape:', pred.shape)
         # print('pred:', pred)
         return pred
+
+
+class CNNModel(nn.Module):
+
+    def __init__(self, vocab_size, embeddings_size, kernel_sizes, kernel_num, dropout):
+        super(CNNModel, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embeddings_size)
+        self.convs = nn.ModuleList([nn.Conv2d(1, kernel_num, (K, embeddings_size)) for K in kernel_sizes])
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(len(kernel_sizes) * kernel_num)
+
+    def forward(self, x):
+        print('initial shape:', x.shape)
+        x = self.embeddings(x)  # (N, max_len, embeddings_size)
+        print('shape after embeddings:', x.shape)
+        x = x.unsqueeze(1)  # (N, 1, max_len, embeddings_size)
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs]  # [(N, kernel_num, max_len)] * len(kernel_sizes)
+        print('length of x after convs:', len(x))
+        print('shape of an element of x after convs:', x[0].shape)
+        x = [F.max_pool1d(i, i.size(2)) for i in x]  # [(N, kernel_num)] * len(kernel_sizes)
+        print('shape of an element of x after pooling:', x[0].shape)
+        x = torch.cat(x, 1)  # (N, len(kernel_sizes) * kernel_num)
+        print('shape of x after cat:', x.shape)
+        x = self.dropout(x)
+        return self.fc(x)  # (N, 1)
