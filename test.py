efa12f16fa879ddd84002232f254e77b9e0afff6
NicolasDutronc <nicolas.dutronc@orange.fr>
2018-11-09 03:31:56 +0100

    refact(): refacto des metrics et comparaison avec dummy model

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import roc_auc_score, roc_curve, mean_squared_error
 import matplotlib.pyplot as plt
+import numpy as np
 
 from dataset import TextDataset
 from models import SimpleModel
@@ -26,7 +27,7 @@ data.index_labels()
 
 print(data[1])
 
-model = SimpleModel(len(vocab), 5)
+model = SimpleModel(len(vocab), 10)
 model.cuda()
 
 train, test = data.split(0.7)
@@ -40,7 +41,7 @@ optimizer = optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
 # loss
 loss = nn.CrossEntropyLoss(torch.DoubleTensor([0.86]))
 
-num_epoch = 5
+num_epoch = 3
 train_losses = []
 test_losses = []
 cmap = get_cmap(num_epoch)
@@ -51,10 +52,10 @@ for i in range(num_epoch):
     for sequence, label in train_loader:
 
         # print(sequence)
-        # print(label)
+        # print(label[0])
 
         sequence = sequence.cuda()
-        label = label.cuda()
+        label = label[0].cuda()
 
         optimizer.zero_grad()
         prediction = model(sequence)
@@ -76,7 +77,7 @@ for i in range(num_epoch):
             test_labels += [float(x) for x in label.cpu().numpy().tolist()[0]]
 
             sequence = sequence.cuda()
-            label = label.cuda()
+            label = label[0].cuda()
 
             prediction = model(sequence)
 
@@ -88,21 +89,30 @@ for i in range(num_epoch):
         # print('test labels:', test_labels)
         # print('test predictions:', test_predictions)
 
-        print('auc:', roc_auc_score(test_labels, test_predictions))
+        print('model auc:', roc_auc_score(test_labels, test_predictions))
+        print('model mse:', mean_squared_error(test_labels, test_predictions))
 
         [fpr, tpr, thr] = roc_curve(test_labels, test_predictions)
+        print('thresholds:', thr)
         print('sensitivity:', tpr)
         print('specificity:', 1 - fpr)
 
         plt.plot(fpr, tpr, color=cmap(i), lw=2, label=i)
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('1 - specificite', fontsize=14)
-        plt.ylabel('Sensibilite', fontsize=14)
-        plt.legend()
 
         gc.collect()
 
+dummy_labels = data.labels
+dummy_predictions = np.ones(len(dummy_labels)) * 0.24
+print('dummy auc:', roc_auc_score(dummy_labels, dummy_predictions))
+print('dummy mse:', mean_squared_error(dummy_labels, dummy_predictions))
+[fpr, tpr, thr] = roc_curve(dummy_labels, dummy_predictions)
+plt.plot(fpr, tpr, label='dummy')
+
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('1 - spécificité', fontsize=14)
+plt.ylabel('Sensibilité', fontsize=14)
+plt.legend()
 plt.show()
 
 print('train losses:', train_losses)
